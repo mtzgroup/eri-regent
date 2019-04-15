4c00be089ee0d1e68f9b814d47f982d4ca3e66df
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2019-04-15 12:19:45 -0700

    Add python benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,7 +3,6 @@
 set -eu
 
 LEGION_OPTIONS="\
--fcuda 0 \
 -ll:cpu 1 -ll:gpu 0 \
 -ll:csize 1024 -ll:fsize 1024"
 
@@ -16,9 +15,9 @@ small-water/h2o_3_6-311g.dat
 small-water/h2o_4_6-311g.dat
 water-boxes/h2o_5_6-311g.dat
 water-boxes/h2o_10_6-311g.dat
-water-boxes/h2o_50_6-311g.dat
-water-boxes/h2o_100_6-311g.dat
 "
+# water-boxes/h2o_50_6-311g.dat
+# water-boxes/h2o_100_6-311g.dat
 # water-boxes/h2o_250_6-311g.dat
 # water-boxes/h2o_500_6-311g.dat
 # water-boxes/h2o_750_6-311g.dat
@@ -27,5 +26,5 @@ water-boxes/h2o_100_6-311g.dat
 
 for FILE in $INPUT_FILES; do
   echo $FILE
-  srun regent top.rg -i "$DATA_DIRECTORY/$FILE" $LEGION_OPTIONS 2> /dev/null | sed -n "s/Coulomb operator: \(.*\) sec/\1/p"
+  regent top.rg -i "$DATA_DIRECTORY/$FILE" $LEGION_OPTIONS 2> /dev/null | sed -n "s/Coulomb operator: \(.*\) sec/\1/p"
 done
