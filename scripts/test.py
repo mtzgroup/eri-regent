7827b35c1816801e5e76caafbb19548ded07b2a6
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2019-10-10 12:05:22 -0700

    Fix test script and add test data

--- scripts/test.py
+++ scripts/test.py
@@ -1,22 +1,8 @@
 test_files = [
-    ("data/h2_6-311g.dat", "data/h2_6-311g_out.dat", 0),
-    ("data/b2_6-311g.dat", "data/b2_6-311g_out.dat", 2),
-    ("data/cl2_6-311g.dat", "data/cl2_6-311g_out.dat", 2),
-    ("data/f2_6-311g.dat", "data/f2_6-311g_out.dat", 2),
-    ("data/li2_6-311g.dat", "data/li2_6-311g_out.dat", 2),
-    ("data/o2_6-311g.dat", "data/o2_6-311g_out.dat", 2),
-    ("data/h2o_6-311g.dat", "data/h2o_6-311g_out.dat", 2),
-    ("data/mg2_6-311g.dat", "data/mg2_6-311g_out.dat", 2),
-    ("data/small-water/h2o_2_6-311g.dat", "data/small-water/h2o_2_6-311g_out.dat", 2),
-    ("data/small-water/h2o_3_6-311g.dat", "data/small-water/h2o_3_6-311g_out.dat", 2),
-    ("data/small-water/h2o_4_6-311g.dat", "data/small-water/h2o_4_6-311g_out.dat", 2),
-    # ("data/ti2_crenbl.dat", "data/ti2_crenbl_out.dat", 4),
-    # ("data/li2_cc-pvtz.dat", "data/li2_cc-pvtz_out.dat", 6),
+    ("data/h2o", "data/h2o/output.dat", 1),
 ]
 
-quick_test_files = [
-    ("data/small-water/h2o_4_6-311g.dat", "data/small-water/h2o_4_6-311g_out.dat", 2)
-]
+quick_test_files = test_files[:1]
 
 RED = "\033[1;31m"
 GREEN = "\033[0;32m"
@@ -32,12 +18,12 @@ if __name__ == "__main__":
 
     data_files = quick_test_files if args.quick else test_files
 
-    for (infile, outfile, L) in data_files:
+    for (indirectory, outfile, L) in data_files:
         # TODO: Test multiple cpu's and partitions
         # TODO: Test gpu and cuda
         try:
             subprocess.check_call(
-                ["regent", "top.rg", "--max_momentum", str(L), "-i", infile, "-v", outfile, "-fflow", "0"],
+                ["regent", "top.rg", "-L", str(L), "-i", infile, "-v", outfile, "-fflow", "0"],
                 cwd="src/",
             )
         except:
